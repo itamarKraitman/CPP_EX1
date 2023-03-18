c4b5f3b477c8631224ec3edda6a478e2ab1e0c84
ubuntu <itamar.kraitman@msmail.ariel.ac.il>
2023-03-18 22:11:48 +0200

    first commit

---
+++ AdptArray.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include "AdptArray.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+// defining the ADT
+typedef struct AdptArray_
+{
+    PElement* addArr;
+    COPY_FUNC copy_func;
+    DEL_FUNC delete_func;
+    PRINT_FUNC print_func;
+    int size;
+}AdptArray_;
+
+
+// creating adt, takes three arguments and retruns NULL id fails 
+PAdptArray CreateAdptArray(COPY_FUNC copy_func, DEL_FUNC del_func ,PRINT_FUNC print_func) {
+
+    PAdptArray adt = (PAdptArray)malloc(sizeof(AdptArray_));
+    if (!adt)
+    {
+        return NULL;
+    }
+
+    adt->addArr = (PElement*)malloc(sizeof(PElement));
+    adt->copy_func = copy_func;
+    adt->delete_func = del_func;
+    adt->print_func = print_func;
+    adt->size= 0;
+
+    return adt;
+}
+
+// free adt alongside all its elements
+void DeleteAdptArray(PAdptArray adt) {
+
+    if (!adt)
+    {
+        return NULL;
+    }
+
+    PElement current; 
+    for (int i = 0; i < adt->size; i++)
+    {
+        current = adt->addArr[i];
+        if (current) { // elements can be NULL
+            adt->delete_func(current);
+        }
+    }
+
+    free(current);
+    free(adt->addArr);
+    free(adt);
+}
+
+Result SetAdptArrayAt(PAdptArray adt, int index, PElement element) {
+
+}
+
+PElement GetAdptArrayAt(PAdptArray adt, int i) {
+
+    if (!adt)
+    {
+        return NULL; 
+    }
+    if (adt->size < i)
+    {
+        return NULL;
+    }
+    if (!adt->addArr[i])
+    {
+        return FAIL;
+    }
+
+    return adt->copy_func(adt->addArr[i]);
+    
+    
+    
+}
+
+int GetAdptArraySize(PAdptArray adt) {
+
+    if (!adt)
+    {
+        return -1;
+    }
+
+    return adt->size;   
+}
+
+void PrintDB(PAdptArray adt) {
+
+}
+
+
+
+
+
+
+
